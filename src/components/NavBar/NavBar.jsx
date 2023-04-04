a4ced88ba8ce8e0a3b01274642b8d22f67c4c047
luhobrecht <luhobrecht@hotmail.com>
2023-04-04 18:06:08 +0200

    Change of NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,10 @@ export const NavBar = () => {
     <div className='navbar'>
     <BrandIcon />
     <div className="nav-buttons">
-    <button className='buttons-navbar'>Productos</button>
+    <button className='buttons-navbar'>Gorras</button>
+    <button className='buttons-navbar'>Remeras</button>
+    <button className='buttons-navbar'>Hoodies</button>
+    <button className='buttons-navbar'>Accesorios</button>
     <button className='buttons-navbar'>Contacto</button>
     <button className='buttons-navbar'>Nosotros</button>
     </div>
